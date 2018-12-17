85eeeca9394bd150994c2f41dbe549ba764eeacd
SPlay <SPlay@DESKTOP-U92BQFG>
2018-12-17 18:32:54 -0200

    Adding contents from video

--- UsingRegexInCSharp/Program.cs
+++ UsingRegexInCSharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace UsingRegexInCSharp
 {
@@ -6,7 +7,19 @@ namespace UsingRegexInCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var regex = @"^[\w]{1,}[@]{1}[\w]{1,}(?>[.{1}][\w]+)+$";
+            var email = "splay@senai.br";
+            var invalid = "SENAI SPlay";
+            //
+            var matches = Regex.Match(email, regex);
+            if(matches.Success)
+            {
+                Console.WriteLine("Valid e-mail");
+            }
+            else
+            {
+                Console.WriteLine("Invalid e-mail");
+            }
         }
     }
 }
